423f60389b4e0bc005fdff73d1d30a0107d39a2e
boyfromgod <manton2007@gmail.com>
2022-05-08 16:27:45 +0300

    adding textures and olezikcoin

--- src/main/java/net/boyfromgod/olezik/OlezikMod.java
+++ src/main/java/net/boyfromgod/olezik/OlezikMod.java
@@ -1,17 +1,18 @@
 package net.boyfromgod.olezik;
 
+import net.boyfromgod.olezik.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OlezikMod implements ModInitializer {
-
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "olezikmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 
+		ModItems.registerModItems();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
